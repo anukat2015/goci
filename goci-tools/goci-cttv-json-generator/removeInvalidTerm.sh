8dcb7c260d529e5a0ded1f06df96d814e9530bcd
Catherine Leroy <cleroy@ebi.ac.uk>
2015-11-09 15:52:37 +0000

    Added a bit of explaination on how to run the script.

--- goci-tools/goci-cttv-json-generator/removeInvalidTerm.sh
+++ goci-tools/goci-cttv-json-generator/removeInvalidTerm.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-#Thiscodeistoremovethejsonstringcontainingefotermsconsideredasinvalid
-#bycttv(notdiseasenorphenotypes).
+#This code is to remove the json string containing efo terms considered as invalid
+#by cttv (not disease nor phenotypes).
+# You need to create a file called ./efoTerms.tab in the directory you're running that script from and add the IRI of the 'invalid' term (one per line). 
+# change the RELEASE_FILE and RELEASE_FILE_COPY variable to match your settings.
+# Will clean that script if this is to be run every months. 
 
 old_IFS=$IFS      # save the field separator
 IFS=$'\n'     # new field separator, the end of line
